0f952d09e627d1c3af6e8e4bbb5ccd98e9701fc6
Rituraj <54928615+ZALER-01@users.noreply.github.com>
2021-11-24 10:48:13 +0530

    Add files via upload

---
+++ hero2/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,15 @@
+
+import { Hero } from "./hero"
+export const HEROES:Hero[]=[
+  {id :1 , name :"nice"},
+  {id:2 , name:'Narco'},
+  {id:3 , name :"Bambasto"},
+  {id:4, name :'magnum'},
+  {id :5,name :'superman'},
+  {id :6 , name :'Arrow'},
+  {id :7, name :"Aqua"},
+  {id:8 , name :"wonder"},
+  {id :9, name :"Synderila"},
+  {id:10, name :'Ironman'}
+]
+
